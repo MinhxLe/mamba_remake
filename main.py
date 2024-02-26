fefc04a7ec2265761a9464648bd6d72c7499ad7a
Minh Le <minh@parafin.com>
2024-02-25 21:17:11 -0800

    minimal model inference on toy dataset in main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+from functools import partial
+import os
+
+from torch import embedding
+from data import Datasets
+
+from model import BatchStackedModel, SSMLayer
+import numpy as np
+import jax
+
+key = jax.random.PRNGKey(0)
+key, rng, train_rng = jax.random.split(key, num=3)
+trainloader, testloader, n_classes, l_max, d_input = Datasets["sin"]()
+
+# initalizing the model
+MODEL_CONFIG = dict(
+    d_model=64,
+    n_layers=10,
+    dropout=0.0,
+    embedding=False,
+    layer=dict(
+        E=1,  # this has to be input size??
+        N=64,
+        l_max=l_max,
+    ),
+)
+model_cls = partial(
+    BatchStackedModel,
+    layer_cls=SSMLayer,
+    d_output=n_classes,
+    classification=False,
+    **MODEL_CONFIG,
+)
+model = model_cls(training=True)
+init_rng, dropout_rng = jax.random.split(rng, num=2)
+init_data = np.array(
+    next(iter(trainloader))[0].numpy()
+)  # need shape of data to initialize
+params = model.init(
+    {"params": init_rng, "dropout": dropout_rng},  # rng initialization
+    np.array(next(iter(trainloader))[0].numpy()),  # need shape of data to initialize
+)
+print(model.apply(params, init_data, rngs=dict(dropout=dropout_rng)))
---
+++ model.py
@@ -0,0 +1,185 @@
+from functools import partial
+import jax
+import jax.numpy as np
+from numpy import who
+from flax import linen as nn
+from jax.nn.initializers import lecun_normal, normal
+from jax.numpy.linalg import eigh, inv, matrix_power
+from jax.scipy.signal import convolve
+
+
+def causal_convolution(u, K, nofft=False):
+    if nofft:
+        return convolve(u, K, mode="full")[: u.shape[0]]
+    else:
+        assert K.shape[0] == u.shape[0]
+        ud = np.fft.rfft(np.pad(u, (0, K.shape[0])))
+        Kd = np.fft.rfft(np.pad(K, (0, u.shape[0])))
+        out = ud * Kd
+        return np.fft.irfft(out)[: u.shape[0]]
+
+
+def scan_SSM(Ab, Bb, Cb, u, x0):
+    # no D b/c does not interact with input state
+    def step(x_k_1, u_k):
+        x_k = Ab @ x_k_1 + Bb @ u_k
+        y_k = Cb @ x_k
+        return x_k, y_k
+
+    return jax.lax.scan(step, x0, u)
+
+
+def K_conv(Ab, Bb, Cb, L):
+    return np.array([(Cb @ matrix_power(Ab, l) @ Bb).reshape() for l in range(L)])
+
+
+class _SSMLayer(nn.Module):
+    E: int
+    N: int
+    l_max: int
+    decode: bool = False
+
+    def setup(self):
+        # SSM parameters
+        self.A = self.param("A", lecun_normal(), (self.N, self.N))
+        self.B = self.param("B", lecun_normal(), (self.N, self.E))
+        self.C = self.param("C", lecun_normal(), (self.E, self.N))
+        self.D = self.param("D", nn.initializers.ones, (self.E,))
+
+        self.K = K_conv(self.A, self.B, self.C, self.l_max)
+
+        # RNN cache for long sequences
+        # self.x_k_1 = self.variable("cache", "cache_x_k", np.zeros, (self.N,))
+
+    def __call__(self, u):
+        if not self.decode:
+            # CNN Mode
+            return causal_convolution(u, self.K) + self.D * u
+        else:
+            # RNN Mode
+            x_k, y_s = scan_SSM(
+                self.A, self.B, self.C, u[:, np.newaxis], self.x_k_1.value
+            )
+            if self.is_mutable_collection("cache"):
+                self.x_k_1.value = x_k
+            return y_s.reshape(-1).real + self.D * u
+
+
+def cloneLayer(layer):
+    # used to have a layer per feature
+    return nn.vmap(
+        layer,
+        in_axes=1,  # over feature dimension
+        out_axes=1,
+        variable_axes={"params": 1, "cache": 1, "prime": 1},
+        split_rngs={"params": True},
+    )
+
+
+# for feature dimension
+SSMLayer = cloneLayer(_SSMLayer)
+
+
+class SequenceBlock(nn.Module):
+    layer_cls: nn.Module
+    layer: dict  # Hyperparameters of inner layer
+    dropout: float
+    d_model: int
+    prenorm: bool = True
+    glu: bool = True
+    training: bool = True
+    decode: bool = False
+
+    def setup(self):
+        self.seq = self.layer_cls(**self.layer, decode=self.decode)
+        self.norm = nn.LayerNorm()
+        self.out = nn.Dense(self.d_model)
+        # gated linear unit
+        if self.glu:
+            self.out2 = nn.Dense(self.d_model)
+        self.drop = nn.Dropout(
+            self.dropout,
+            broadcast_dims=[0],
+            deterministic=not self.training,
+        )
+
+    def __call__(self, x):
+        skip = x
+        if self.prenorm:
+            x = self.norm(x)
+        x = self.seq(x)
+        x = self.drop(nn.gelu(x))
+        if self.glu:
+            x = self.out(x) * jax.nn.sigmoid(self.out2(x))
+        else:
+            x = self.out(x)
+        x = skip + self.drop(x)
+        if not self.prenorm:
+            x = self.norm(x)
+        return x
+
+
+class Embedding(nn.Embed):
+    num_embeddings: int
+    features: int
+
+    @nn.compact
+    def __call__(self, x):
+        y = nn.Embed(self.num_embeddings, self.features)(x[..., 0])
+        return np.where(x > 0, y, 0.0)  # x is onehot
+
+
+class StackedModel(nn.Module):
+    layer_cls: nn.Module
+    layer: dict  # Extra arguments to pass into layer constructor
+    d_output: int
+    d_model: int
+    n_layers: int
+    prenorm: bool = True
+    dropout: float = 0.0
+    embedding: bool = False  # Use nn.Embed instead of nn.Dense encoder
+    classification: bool = False
+    training: bool = True
+    decode: bool = False  # Probably should be moved into layer_args
+
+    def setup(self):
+        if self.embedding:
+            self.encoder = Embedding(self.d_output, self.d_model)
+        else:
+            self.encoder = nn.Dense(self.d_model)
+        self.decoder = nn.Dense(self.d_output)
+        self.layers = [
+            SequenceBlock(
+                layer_cls=self.layer_cls,
+                layer=self.layer,
+                prenorm=self.prenorm,
+                d_model=self.d_model,
+                dropout=self.dropout,
+                training=self.training,
+                decode=self.decode,
+            )
+            for _ in range(self.n_layers)
+        ]
+
+    def __call__(self, x):
+        if not self.classification:
+            if not self.embedding:
+                x = x / 255.0  # Normalize
+            if not self.decode:
+                x = np.pad(x[:-1], [(1, 0), (0, 0)])
+        x = self.encoder(x)
+        for layer in self.layers:
+            x = layer(x)
+        if self.classification:
+            x = np.mean(x, axis=0)
+        x = self.decoder(x)
+        return nn.log_softmax(x, axis=-1)
+
+
+BatchStackedModel = nn.vmap(
+    StackedModel,
+    in_axes=0,
+    out_axes=0,
+    variable_axes={"params": None, "dropout": None, "cache": 0, "prime": None},
+    split_rngs={"params": False, "dropout": True},
+)
